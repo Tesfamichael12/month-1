16c812ee840b940e1425d2ce74826f0c61a815ab
Tesfamichael12 <tesfamichael132@gmail.com>
2025-02-13 19:09:02 +0300

    week-2

---
+++ week-2/largest.py
@@ -0,0 +1,6 @@
+def largest(nums):
+    return max(nums)
+
+arr = [3,3,4,2,4,2]
+
+print(largest(arr))
---
+++ week-2/list_sum.py
@@ -0,0 +1,5 @@
+def list_sum(nums):
+    return sum(nums)
+
+arr = [3, 4, 2, 4, 5]
+print(list_sum(arr))
